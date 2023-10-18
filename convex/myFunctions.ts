e6760127b35e8333ee128008b22acff13ca145da
Anjana Vakil <contact@anjana.dev>
2023-10-18 17:22:04 +0200

    messing with functions

--- convex/myFunctions.ts
+++ convex/myFunctions.ts
@@ -5,27 +5,36 @@ import { api } from "./_generated/api";
 // Write your Convex functions in any file inside this directory (`convex`).
 // See https://docs.convex.dev/functions for more.
 
-// You can read data from the database via a query:
-export const listNumbers = query({
-  // Validators for arguments.
-  args: {
-    count: v.number(),
-  },
+// You can read data from the database via a query function:
+export const listSteps = query({
+  // Validators for arguments. (In this case, we have no arguments.)
+  args: {},
 
-  // Query implementation.
-  handler: async (ctx, args) => {
+  // Query function implementation.
+  handler: async (ctx) => {
     //// Read the database as many times as you need here.
     //// See https://docs.convex.dev/database/reading-data.
-    const numbers = await ctx.db.query("numbers").take(args.count);
-    return numbers.map((number) => number.value);
+    return await ctx.db.query("steps").collect();
   },
 });
 
-// You can write data to the database via a mutation:
-export const addNumber = mutation({
+// You can write data to the database via a mutation function:
+export const getHacking = mutation({
+  // Validators for arguments.
+  args: {},
+  handler: async (ctx) => {
+    for (const step of steps) {
+      await ctx.db.insert("steps", step);
+    }
+  },
+});
+
+export const addStep = mutation({
   // Validators for arguments.
   args: {
-    value: v.number(),
+    number: v.number(),
+    name: v.string(),
+    label: v.string(),
   },
 
   // Mutation implementation.
@@ -34,38 +43,47 @@ export const addNumber = mutation({
     //// Mutations can also read from the database like queries.
     //// See https://docs.convex.dev/database/writing-data.
 
-    const id = await ctx.db.insert("numbers", { value: args.value });
+    const { number, name, label } = args;
 
+    const id = await ctx.db.insert("steps", {
+      number,
+      name,
+      label,
+      done: false,
+    });
+
+    // Optionally, write to the server logs from within your function.
     console.log("Added new document with id:", id);
+
     // Optionally, return a value from your mutation.
-    // return id;
+    return id;
   },
 });
 
-// You can fetch data from and send data to third-party APIs via an action:
-export const myAction = action({
-  // Validators for arguments.
-  args: {
-    first: v.number(),
-    second: v.string(),
-  },
+// // You can fetch data from and send data to third-party APIs via an action:
+// export const myAction = action({
+//   // Validators for arguments.
+//   args: {
+//     first: v.number(),
+//     second: v.string(),
+//   },
 
-  // Action implementation.
-  handler: async (ctx, args) => {
-    //// Use the browser-like `fetch` API to send HTTP requests.
-    //// See https://docs.convex.dev/functions/actions#calling-third-party-apis-and-using-npm-packages.
-    // const response = await ctx.fetch("https://api.thirdpartyservice.com");
-    // const data = await response.json();
+//   // Action implementation.
+//   handler: async (ctx, args) => {
+//     //// Use the browser-like `fetch` API to send HTTP requests.
+//     //// See https://docs.convex.dev/functions/actions#calling-third-party-apis-and-using-npm-packages.
+//     // const response = await ctx.fetch("https://api.thirdpartyservice.com");
+//     // const data = await response.json();
 
-    //// Query data by running Convex queries.
-    const data = await ctx.runQuery(api.myFunctions.listNumbers, {
-      count: 10,
-    });
-    console.log(data);
+//     //// Query data by running Convex queries.
+//     const data = await ctx.runQuery(api.myFunctions.listNumbers, {
+//       count: 10,
+//     });
+//     console.log(data);
 
-    //// Write data by running Convex mutations.
-    await ctx.runMutation(api.myFunctions.addNumber, {
-      value: args.first,
-    });
-  },
-});
+//     //// Write data by running Convex mutations.
+//     await ctx.runMutation(api.myFunctions.addNumber, {
+//       value: args.first,
+//     });
+//   },
+// });
