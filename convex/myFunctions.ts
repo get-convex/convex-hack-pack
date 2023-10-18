cf3f6c08cba5e51d987d5f44c88dafed1af17627
Anjana Vakil <contact@anjana.dev>
2023-10-18 17:29:42 +0200

    Basic query/mutation functions for starter steps

--- convex/myFunctions.ts
+++ convex/myFunctions.ts
@@ -22,68 +22,69 @@ export const listSteps = query({
 export const getHacking = mutation({
   // Validators for arguments.
   args: {},
+
+  // Mutation function implementation.
   handler: async (ctx) => {
-    for (const step of steps) {
-      await ctx.db.insert("steps", step);
-    }
-  },
-});
-
-export const addStep = mutation({
-  // Validators for arguments.
-  args: {
-    number: v.number(),
-    name: v.string(),
-    label: v.string(),
-  },
-
-  // Mutation implementation.
-  handler: async (ctx, args) => {
     //// Insert or modify documents in the database here.
     //// Mutations can also read from the database like queries.
     //// See https://docs.convex.dev/database/writing-data.
+    const steps = [
+      {
+        number: 0,
+        name: "dashboard",
+        label: "Edit data manually in the Convex Dashboard",
+        done: false,
+      },
+      {
+        number: 1,
+        name: "backend",
+        label: "Edit `convex/myFunctions.ts` to change your backend",
+        done: false,
+      },
+      {
+        number: 2,
+        name: "frontend",
+        label: "Edit `src/App.tsx` to change your frontend",
+        done: false,
+      },
+    ];
 
-    const { number, name, label } = args;
-
-    const id = await ctx.db.insert("steps", {
-      number,
-      name,
-      label,
-      done: false,
-    });
-
-    // Optionally, write to the server logs from within your function.
-    console.log("Added new document with id:", id);
+    const documentIds: string[] = [];
+    for (const step of steps) {
+      // Optionally, capture the ID of the newly created document
+      const id = await ctx.db.insert("steps", step);
+      console.log("Added new document with id:", id);
+    }
 
     // Optionally, return a value from your mutation.
-    return id;
+    return documentIds;
   },
 });
 
-// // You can fetch data from and send data to third-party APIs via an action:
-// export const myAction = action({
-//   // Validators for arguments.
-//   args: {
-//     first: v.number(),
-//     second: v.string(),
-//   },
+// You can fetch data from and send data to third-party APIs via an action:
+export const myAction = action({
+  // Validators for arguments.
+  args: {
+    first: v.number(),
+    second: v.string(),
+  },
 
-//   // Action implementation.
-//   handler: async (ctx, args) => {
-//     //// Use the browser-like `fetch` API to send HTTP requests.
-//     //// See https://docs.convex.dev/functions/actions#calling-third-party-apis-and-using-npm-packages.
-//     // const response = await ctx.fetch("https://api.thirdpartyservice.com");
-//     // const data = await response.json();
+  // Action implementation.
+  handler: async (ctx, args) => {
+    //// Use the browser-like `fetch` API to send HTTP requests.
+    //// See https://docs.convex.dev/functions/actions#calling-third-party-apis-and-using-npm-packages.
+    // const response = await ctx.fetch("https://api.thirdpartyservice.com");
+    // const data = await response.json();
 
-//     //// Query data by running Convex queries.
-//     const data = await ctx.runQuery(api.myFunctions.listNumbers, {
-//       count: 10,
-//     });
-//     console.log(data);
+    //// Query data by running Convex queries.
+    const data = await ctx.runQuery(api.myFunctions.listNumbers, {
+      count: 10,
+    });
+    console.log(data);
 
-//     //// Write data by running Convex mutations.
-//     await ctx.runMutation(api.myFunctions.addNumber, {
-//       value: args.first,
-//     });
-//   },
-// });
+    //// Write data by running Convex mutations.
+    await ctx.runMutation(api.myFunctions.addNumber, {
+      value: args.first,
+    });
+  },
+});
--- src/App.tsx
+++ src/App.tsx
@@ -6,82 +6,86 @@ import { api } from "../convex/_generated/api";
 const checkboxClass = "flex items-center gap-2";
 
 function App() {
-  const numbers = useQuery(api.myFunctions.listNumbers, { count: 10 });
-  const addNumber = useMutation(api.myFunctions.addNumber);
+  const steps = useQuery(api.myFunctions.listSteps);
+  const getHacking = useMutation(api.myFunctions.getHacking);
 
   return (
     <main className="container max-w-2xl flex flex-col gap-8">
       <h1 className="text-4xl font-extrabold my-8 text-center">
         Get Hacking with Convex
       </h1>
-      {/* <p>
-        Click the button and open this page in another window - this data is
-        persisted in the Convex cloud database!
-      </p>
-      <p>
-        <Button
-          onClick={() => {
-            void addNumber({ value: Math.floor(Math.random() * 10) });
-          }}
-        >
-          Add a random number
-        </Button>
-      </p>
-      <p>
-        Numbers:{" "}
-        {numbers?.length === 0
-          ? "Click the button!"
-          : numbers?.join(", ") ?? "..."}
-      </p> */}
-      <div className={checkboxClass}>
-        <Checkbox id="dashboard" />
-        <label htmlFor="dashboard">
-          Visit{" "}
-          <a
-            className="font-medium text-primary underline underline-offset-4"
-            target="_blank"
-            href="https://dashboard.convex.dev/"
-          >
-            dashboard.convex.dev
-          </a>{" "}
-          to view & edit your data
-        </label>
-      </div>
-      <div className={checkboxClass}>
-        <Checkbox id="backend" />
-        <label htmlFor="backend">
-          Edit{" "}
-          <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
-            convex/myFunctions.ts
-          </code>{" "}
-          to change your backend
-        </label>
-      </div>
+      {steps && steps.length === 0 ? (
+        <>
+          <p>Ready to start building rapid prototypes with Convex?</p>
+          <p>
+            <Button
+              onClick={() => {
+                getHacking();
+              }}
+            >
+              Let's go!
+            </Button>
+          </p>
+        </>
+      ) : (
+        <>
+          {steps?.map(({ name, label, done }) => (
+            <div className={checkboxClass}>
+              <Checkbox id={name} checked={done} />
+              <label htmlFor={name}>{label}</label>
+            </div>
+          ))}
+          {/* <div className={checkboxClass}>
+            <Checkbox id="dashboard" />
+            <label htmlFor="dashboard">
+              Visit{" "}
+              <a
+                className="font-medium text-primary underline underline-offset-4"
+                target="_blank"
+                href="https://dashboard.convex.dev/"
+              >
+                dashboard.convex.dev
+              </a>{" "}
+              to view & edit your data
+            </label>
+          </div>
+          <div className={checkboxClass}>
+            <Checkbox id="backend" />
+            <label htmlFor="backend">
+              Edit{" "}
+              <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+                convex/myFunctions.ts
+              </code>{" "}
+              to change your backend
+            </label>
+          </div>
 
-      <div className={checkboxClass}>
-        <Checkbox id="frontend" />
-        <label htmlFor="frontend">
-          Edit{" "}
-          <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
-            src/App.tsx
-          </code>{" "}
-          to change your frontend
-        </label>
-      </div>
-      <div className={checkboxClass}>
-        <Checkbox id="docs" />
-        <label htmlFor="docs">
-          Check out the{" "}
-          <a
-            className="font-medium text-primary underline underline-offset-4"
-            target="_blank"
-            href="https://docs.convex.dev/home"
-          >
-            Convex docs
-          </a>{" "}
-          to learn more
-        </label>
-      </div>
+          <div className={checkboxClass}>
+            <Checkbox id="frontend" />
+            <label htmlFor="frontend">
+              Edit{" "}
+              <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+                src/App.tsx
+              </code>{" "}
+              to change your frontend
+            </label>
+          </div>
+          <div className={checkboxClass}>
+            <Checkbox id="docs" />
+            <label htmlFor="docs">
+              Check out the{" "}
+              <a
+                className="font-medium text-primary underline underline-offset-4"
+                target="_blank"
+                href="https://docs.convex.dev/home"
+              >
+                Convex docs
+              </a>{" "}
+              to learn more
+            </label>
+          </div> */}
+        </>
+      )}
     </main>
   );
 }
