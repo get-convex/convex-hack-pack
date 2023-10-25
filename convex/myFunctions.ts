3b21122d84ec8a5d506f4a2b02bd4e5c90eb6363
Anjana Vakil <contact@anjana.dev>
2023-10-25 15:50:49 +0200

    Reformat

--- convex/myFunctions.ts
+++ convex/myFunctions.ts
@@ -1,6 +1,6 @@
-import { v } from "convex/values";
-import { query, mutation, action } from "./_generated/server";
-import { api } from "./_generated/api";
+import { v } from "convex/values"
+import { query, mutation, action } from "./_generated/server"
+import { api } from "./_generated/api"
 
 // Write your Convex functions in any file inside this directory (`convex`).
 // See https://docs.convex.dev/functions for more.
@@ -12,11 +12,11 @@ export const listIdeas = query({
 
   // Query function implementation.
   handler: async (ctx, args) => {
-    //// Read the database as many times as you need here.
-    //// See https://docs.convex.dev/database/reading-data.
-    return await ctx.db.query("ideas").collect();
+    // Read the database as many times as you need here.
+    // See https://docs.convex.dev/database/reading-data.
+    return await ctx.db.query("ideas").collect()
   },
-});
+})
 
 // You can write data to the database via a mutation function:
 export const saveIdea = mutation({
@@ -28,17 +28,17 @@ export const saveIdea = mutation({
 
   // Mutation function implementation.
   handler: async (ctx, args) => {
-    //// Insert or modify documents in the database here.
-    //// Mutations can also read from the database like queries.
-    //// See https://docs.convex.dev/database/writing-data.
+    // Insert or modify documents in the database here.
+    // Mutations can also read from the database like queries.
+    // See https://docs.convex.dev/database/writing-data.
 
     // Optionally, capture the ID of the newly created document
-    const id = await ctx.db.insert("ideas", args);
+    const id = await ctx.db.insert("ideas", args)
 
     // Optionally, return a value from your mutation.
-    return id;
+    return id
   },
-});
+})
 
 // You can fetch data from and send data to third-party APIs via an action:
 export const fetchRandomIdea = action({
@@ -47,18 +47,18 @@ export const fetchRandomIdea = action({
 
   // Action implementation.
   handler: async (ctx) => {
-    //// Use the browser-like `fetch` API to send HTTP requests.
-    //// See https://docs.convex.dev/functions/actions#calling-third-party-apis-and-using-npm-packages.
-    const response = await fetch("https://appideagenerator.com/call.php");
-    const idea = await response.text();
+    // Use the browser-like `fetch` API to send HTTP requests.
+    // See https://docs.convex.dev/functions/actions#calling-third-party-apis-and-using-npm-packages.
+    const response = await fetch("https://appideagenerator.com/call.php")
+    const idea = await response.text()
 
-    // //// Write or query data by running Convex mutations/queries from within an action
+    // Write or query data by running Convex mutations/queries from within an action
     await ctx.runMutation(api.myFunctions.saveIdea, {
       idea: idea.trim(),
       random: true,
-    });
+    })
 
     // Optionally, return a value from your action
-    return idea;
+    return idea
   },
-});
+})
